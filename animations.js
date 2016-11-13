c6d530e32e41db0dcfb98c9e614c635958756fa4
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2016-11-13 13:07:17 -0700

    fixed character count colors and tweet button

--- animations.js
+++ animations.js
@@ -1,3 +1,43 @@
 $(document).ready(function() {
-  $('#char-count').hide();
+  $('#tweet-controls').hide();
+
+  $('.tweet-compose').on('click', function() {
+    $('#tweet-controls').show();
+  });
+
+  $('.tweet-compose').on('click', function() {
+    $('.tweet-compose').height("5em");
+    $('#tweet-submit').prop('disabled',true);
+  });
+
+
+  $('.tweet-compose').keyup(function() {
+      var charCount = 140-$('.tweet-compose').val().length;
+      $('#char-count').html(charCount);
+
+      if (charCount <= 10) {
+        // alert("less than ten");
+        $('#char-count').css("color","red");
+      }
+      else {
+        $('#char-count').css("color","#AFAFAF");
+      }
+      if (charCount  <= 139 && charCount>= 0) {
+        // alert("140characters")
+        $('#tweet-submit').prop('disabled',false);
+      }
+      else {
+        $('#tweet-submit').prop('disabled',true);
+      }
+  });
+  $('#tweet-submit').click(function() {
+      var $finishedtweet = $('<span>SIDSIDSID</span>')
+      $('#stream').prepend($finishedtweet);
+  })
+
+
 });
+
+
+// width: 100%;
+// height: 2.5em;
